568bf55aa4bfbb823ca5350a6e04ec8d3f12044f
dmbell <dougmbell@gmail.com>
2017-04-01 12:19:19 -0400

    Add character counting via dictionary and dictionary sorting

--- barchart.py
+++ barchart.py
@@ -1,5 +1,5 @@
 import re, string; alphanum = re.compile('[\W_]+', re.UNICODE)
-
+from collections import OrderedDict
 
 def normalize_file(file):
 	list = []
@@ -9,7 +9,23 @@ def normalize_file(file):
 			list.append(alphanum.sub('', str(line)).lower())
 	return list
 
+def calc_freq(line, freqs):
+	for char in line:
+		if char in freqs:
+			freqs[char] = freqs[char] + 1
+		else:
+			freqs[char] = 1
+
 lines = normalize_file('ptresume.txt')
 
+freqs = {}
+
 for line in lines:
-	print(line)
+	calc_freq(line, freqs)
+
+freqs = OrderedDict(sorted(freqs.items()))
+
+print("Number of items in dict: " + str(len(freqs)))
+
+for char, freq in freqs.items():
+	print(char + ":" + str(freq))
