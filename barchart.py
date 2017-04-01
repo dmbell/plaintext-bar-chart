c4b0072eb746196c6ed1959cb96e930f6cb92336
dmbell <dougmbell@gmail.com>
2017-04-01 13:54:32 -0400

    Add matplotlib, clean up functions, and basic bar chart working

--- barchart.py
+++ barchart.py
@@ -1,5 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plot
 from collections import OrderedDict
-import re, string; alphanum = re.compile('[\W_]+', re.UNICODE)
+import re, string; alphanum = re.compile("[\W_]+", re.UNICODE)
 
 def normalize_file(file):
 	list = []
@@ -9,7 +11,7 @@ def normalize_file(file):
 			list.append(alphanum.sub('', str(line)).lower())
 	return list
 
-def calc_freq(line, freqs):
+def calc_frequencies(line, freqs):
 	for char in line:
 		if char in freqs:
 			freqs[char] = freqs[char] + 1
@@ -23,16 +25,27 @@ def reorder_nums(freqs):
 			del freqs[str(i)]
 			freqs[str(i)] = v
 
-lines = normalize_file('ptresume.txt')
+def create_dict(lines):
+	freqs = {}
+	for line in lines:
+		calc_frequencies(line, freqs)
+	freqs = OrderedDict(sorted(freqs.items()))
+	reorder_nums(freqs)
+	return freqs
 
-freqs = {}
+def plot_dict(freqs):
+	y_pos = np.arange(len(freqs))
+	plot.bar(y_pos, list(freqs.values()), align="center", alpha=0.5)
+	plot.xticks(y_pos, list(freqs.keys()))
+	plot.ylabel("Frequency")
+	plot.xlabel("Character")
+	plot.title("Alphanumeric Character Frequency")
+	plot.show()
 
-for line in lines:
-	calc_freq(line, freqs)
+def print_dict(freqs):
+	for char, freq in freqs.items():
+		print(char + ":" + str(freq))
 
-freqs = OrderedDict(sorted(freqs.items()))
-
-reorder_nums(freqs)
-
-for char, freq in freqs.items():
-	print(char + ":" + str(freq))
+lines = normalize_file("ptresume.txt")
+freqs = create_dict(lines)
+plot_dict(freqs)
