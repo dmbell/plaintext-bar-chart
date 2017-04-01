b88b329d2e82f587dac3f2d13a85a7281266a5ea
dmbell <dougmbell@gmail.com>
2017-04-01 11:52:01 -0400

    Add non-alphanum stripping and normalizing letters

--- barchart.py
+++ barchart.py
@@ -1,3 +1,15 @@
-r = open('ptresume.txt', 'r')
-for line in r:
-	print line + '\n'
+import re, string; alphanum = re.compile('[\W_]+', re.UNICODE)
+
+
+def normalize_file(file):
+	list = []
+	with open(file) as file_input:
+		lines = filter(None, (line.rstrip() for line in file_input))
+		for line in lines:
+			list.append(alphanum.sub('', str(line)).lower())
+	return list
+
+lines = normalize_file('ptresume.txt')
+
+for line in lines:
+	print(line)
