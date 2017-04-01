5491afe3ad0b440b63eb833deadc650331cbf8f5
dmbell <dougmbell@gmail.com>
2017-04-01 14:11:15 -0400

    Add file/function documentation

--- barchart.py
+++ barchart.py
@@ -1,9 +1,25 @@
+'''
+barchart.py
+Purpose: Simple bar chart plot of alphanumeric characters in a text file using matplotlib
+Version. 0.1
+Author: Douglas Bell
+'''
+
 import numpy as np
 import matplotlib.pyplot as plot
 from collections import OrderedDict
 import re, string; alphanum = re.compile("[\W_]+", re.UNICODE)
 
 def normalize_file(file):
+	'''
+	Return a list of all lines in a file with blank spaces and non-alphanumeric characters removed in lowercase
+
+	Args:
+		file: Name of file to normalize
+	Returns:
+		list: List of normalized strings
+	'''
+
 	list = []
 	with open(file) as file_input:
 		lines = filter(None, (line.rstrip() for line in file_input))
@@ -12,6 +28,13 @@ def normalize_file(file):
 	return list
 
 def calc_frequencies(line, freqs):
+	'''
+	Calculate the frequency of a character in a line with the given dictionary
+
+	Args:
+		line: Line of characters to count
+		freqs: Dictionary where count of individual characters is being tracked
+	'''
 	for char in line:
 		if char in freqs:
 			freqs[char] = freqs[char] + 1
@@ -19,6 +42,13 @@ def calc_frequencies(line, freqs):
 			freqs[char] = 1
 
 def reorder_nums(freqs):
+	'''
+	Move 0-9, if present, in a pre-sorted given OrderedDict to the end of the dictionary
+
+	Args:
+		freqs: Pre-sorted (0-9, a-z) OrderedDictionary
+	'''
+
 	for i in range(10):
 		if str(i) in freqs:
 			v = freqs[str(i)]
@@ -26,6 +56,16 @@ def reorder_nums(freqs):
 			freqs[str(i)] = v
 
 def create_dict(lines):
+	'''
+	Create a dictionary of <character, frequency> pairs from given list of strings
+
+	Args:
+		lines: List of strings of which to calculate frequency of characters
+
+	Returns:
+		freqs: OrderedDict of <char, freq> pairs
+	'''
+
 	freqs = {}
 	for line in lines:
 		calc_frequencies(line, freqs)
@@ -34,6 +74,13 @@ def create_dict(lines):
 	return freqs
 
 def plot_dict(freqs):
+	'''
+	Plot a basic bar chart using matplotlib of given dictionary's <k, v> pairs
+
+	Args:
+		freqs: Dictionary to plot
+	'''
+
 	y_pos = np.arange(len(freqs))
 	plot.bar(y_pos, list(freqs.values()), align="center", alpha=0.5)
 	plot.xticks(y_pos, list(freqs.keys()))
@@ -43,6 +90,10 @@ def plot_dict(freqs):
 	plot.show()
 
 def print_dict(freqs):
+	'''
+	Print dictionary <k, v> pairs for testing purposes
+	'''
+	
 	for char, freq in freqs.items():
 		print(char + ":" + str(freq))
 
